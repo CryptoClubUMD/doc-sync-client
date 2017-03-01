e5f38801f02aa42fb143349287cb9f7885906589
Jakson Kallio <jakson@protonmail.com>
2017-03-01 10:52:13 -0600

    Document getters and stubs

--- src/Document.cpp
+++ src/Document.cpp
@@ -1,2 +1,17 @@
-#include <string>
-#include "Commit.h"
+#include "Document.h"
+
+void Document::rollback(std::string timeline_hash){
+
+}
+
+void Document::bringup(){
+
+}
+
+std::string Document::getHeadHash() const{
+	return head_hash;
+}
+
+std::string Document::getDocumentLocation() const{
+	return document_location;
+}
