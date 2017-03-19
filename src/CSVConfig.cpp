19ac19b0645a34d121f85b899ba4d0bbc8185c24
Jakson Kallio <jakson@protonmail.com>
2017-03-18 23:40:14 -0500

    Debugging

--- src/CSVConfig.cpp
+++ src/CSVConfig.cpp
@@ -1,31 +1,34 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <sstream>
+
+#include "CSVConfig.h"
 
 CSVConfig::CSVConfig(std::string rlocation){
 	std::ifstream filehook;
 	std::string line;
 	filehook.open(rlocation.c_str());
 
-	while(getline(filehook, line)){
+	while(std::getline(filehook, line)){
 		rows.push_back(commaExplode(line));
 	}
 }
 
-std::string CSVConfig::getValue(unsigned int row, unsigned int column){
-	return "value";
+std::string CSVConfig::getValue(unsigned int row, unsigned int column) const{
+	return rows[row][column];
 }
 
 unsigned int CSVConfig::rowCount() const{
 	return 0;
 }
 
-std::vector<std::string> commaExplode(std::string line) const{
-	std::stringstream content(line);
+std::vector<std::string> CSVConfig::commaExplode(std::string line) const{
+	std::stringstream rawcontent(line);
 	std::vector<std::string> values;
 	std::string segment;
 
-	while(std::getline(content, segment, ",")){
+	while(std::getline(rawcontent, segment, ',')){
 		values.push_back(segment);
 	}
 
